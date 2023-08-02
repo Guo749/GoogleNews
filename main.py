ced93aa3d6bb6156abdaa09ba2853878b65eb3cd
Guo749 <venturekwok@gmail.com>
2023-08-01 22:23:16 -0700

    result looks promising with 0.94, previously I tested using unrelevant data, it is 0.3

--- main.py
+++ main.py
@@ -26,14 +26,14 @@ if __name__ == '__main__':
     # Input paragraphs
     # paragraph1 = "The computer wouldn't start. She banged on the side and tried again. Nothing. She lifted it up and dropped it to the table. Still nothing. She banged her closed fist against the top. It was at this moment she saw the irony of trying to fix the machine with violence.h"
     # paragraph2 = "Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing."
-    paragraph1 = "Definitely not revelent is the first paragraph"
+    paragraph1 = "The weather today is beautiful with clear blue skies"
     token1 = preprocess_paragraph(paragraph1)
-    paragraph2 = "Hello world who am I"
+    paragraph2 = "Today weather is lovely featuring bright blue skies"
     token2 = preprocess_paragraph(paragraph2)
 
     # Preprocess and convert paragraphs into vectors using Word2Vec
-    vector1 = sum(word2vec_model[token] for token in token1 / len(token1))
-    vector2 = sum(word2vec_model[token] for token in token2 / len(token2))
+    vector1 = sum(word2vec_model[token] for token in token1) / len(token1)
+    vector2 = sum(word2vec_model[token] for token in token2) / len(token2)
 
     # Calculate the cosine similarity between the two vectors
     similarity = cosine_similarity(vector1.reshape(1, -1), vector2.reshape(1, -1))[0][0]
